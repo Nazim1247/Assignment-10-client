f411e4ed7d2c5f46c40311791ef1048faf9f7aa4
nazim uddin <mdnajim1247@gmail.com>
2024-12-03 21:40:14 +0600

    project setup done

---
+++ src/components/Root.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Navbar from './Navbar';
+import Footer from './Footer';
+
+const Root = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <div>
+            <Outlet></Outlet>
+            </div>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Root;
