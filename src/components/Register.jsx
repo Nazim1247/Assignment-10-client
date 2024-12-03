afb8fef3a7c0a4e41abc3ca1ee23512d38ff3b81
nazim uddin <mdnajim1247@gmail.com>
2024-12-03 23:35:39 +0600

    register page create done

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,9 +1,62 @@
 import React from 'react';
 
 const Register = () => {
+    const handleRegister = (e)=>{
+        e.preventDefault();
+        const name = e.target.name.value;
+        const email = e.target.email.value;
+        const photo = e.target.photo.value;
+        const password = e.target.password.value;
+        console.log(name,photo,email,password)
+    }
     return (
         <div>
-            register
+            <div className="hero py-12">
+  <div className="hero-content flex-col w-1/2 mx-auto">
+    <div className="text-center lg:text-left">
+      <h1 className="text-2xl font-bold">Register now!</h1>
+    </div>
+    <div className="card bg-base-100 w-full shadow-xl">
+      <form onSubmit={handleRegister} className="card-body">
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Name</span>
+          </label>
+          <input type="text"
+          name='name'
+          placeholder="name" className="input input-bordered" required />
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Email</span>
+          </label>
+          <input type="email"
+          name='email'
+          placeholder="email" className="input input-bordered" required />
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Photo url</span>
+          </label>
+          <input type="text"
+          name='photo'
+          placeholder="photo" className="input input-bordered" required />
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Password</span>
+          </label>
+          <input type="password"
+          name='password'
+          placeholder="password" className="input input-bordered" required />
+        </div>
+        <div className="form-control mt-6">
+          <button className="btn btn-primary">Register</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
         </div>
     );
 };
