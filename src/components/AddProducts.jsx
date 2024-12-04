d027cb1f815a379c1a6d0eec06946d8086e2185c
nazim uddin <mdnajim1247@gmail.com>
2024-12-04 16:10:34 +0600

    add product page create done

--- src/components/AddProducts.jsx
+++ src/components/AddProducts.jsx
@@ -1,52 +1,17 @@
 import React from 'react';
-import Swal from 'sweetalert2';
 
-const AddEquipment = () => {
+const AddProducts = () => {
     const handleAddProduct = (e)=>{
         e.preventDefault();
-        const form = e.target;
-        const name = form.name.value;
-        const rating = form.rating.value;
-        const category = form.category.value;
-        const customization = form.customization.value;
-        const userEmail = form.userEmail.value;
-        const stockStatus = form.stockStatus.value;
-        const price = form.price.value;
-        const description = form.description.value;
-        const processingTime = form.processingTime.value;
-        const userName = form.userName.value;
-        const photo = form.photo.value;
-        const newProduct = {name,rating,category,customization,userEmail,stockStatus,price,description,processingTime,userName,photo,};
-        console.log(newProduct)
-
-        // send data to the server
-        fetch('http://localhost:5000/products', {
-            method: 'POST',
-            headers: {
-                'content-type': 'application/json'
-            },
-            body: JSON.stringify(newProduct)
-        })
-        .then(res => res.json())
-        .then(data => {
-            console.log(data)
-            if(data.insertedId){
-                Swal.fire({
-                    title: 'Success!',
-                    text: 'Do you want to continue',
-                    icon: 'success',
-                    confirmButtonText: 'Cool'
-                  })
-            }
-        })
     }
     return (
         <div>
-            <div className='lg:w-3/4 mx-auto py-8'>
+            <div className='lg:w-3/4 mx-auto'>
             <div className="text-center p-10">
-                <h1 className="text-3xl font-bold">Add Product!</h1>
+                <h1 className="text-5xl font-bold">Add Product!</h1>
                 <p className="py-6">
-                Add new sports products to our inventory with details like images, name, category, price, stock status, and customization options. User details are auto-filled for accountability. Secure and accessible only to authenticated users, ensuring efficient catalog management.
+                    Provident cupiditate voluptatem et in.Quaerat fugiat ut assumenda excepturi exercitationem
+                    quasi. In deleniti eaque aut repudiandae et a id nisi.
                 </p>
             </div>
             <div className="card bg-base-100 w-full shrink-0 shadow-2xl">
@@ -61,9 +26,9 @@ const AddEquipment = () => {
                         </div>
                         <div className="form-control flex-1">
                             <label className="label">
-                                <span className="label-text">Stock Status</span>
+                                <span className="label-text">Chef</span>
                             </label>
-                            <input type="text" name='stockStatus' placeholder="Stock Status" className="input input-bordered" required />
+                            <input type="text" name='chef' placeholder="chef name" className="input input-bordered" required />
                         </div>
                     </div>
                     {/* form second row */}
@@ -127,6 +92,13 @@ const AddEquipment = () => {
                         </div>
                     </div>
                     {/* form 6th row */}
+                    <div className='flex flex-col lg:flex-row gap-5'>
+                        <div className="form-control flex-1">
+                            <label className="label">
+                                <span className="label-text">Stock Status</span>
+                            </label>
+                            <input type="text" name='stockStatus' placeholder="Stock Status" className="input input-bordered" required />
+                        </div>
                         <div className="form-control">
                         <label className="label">
                             <span className="label-text">Photo URL</span>
@@ -134,6 +106,9 @@ const AddEquipment = () => {
                         <input type="text" name='photo' placeholder="Photo url" className="input input-bordered" required />
 
                     </div>
+                    </div>
+
+
                     
                     <div className="form-control mt-6">
                         <button className="btn btn-primary">Add Product</button>
@@ -145,4 +120,4 @@ const AddEquipment = () => {
     );
 };
 
-export default AddEquipment;
+export default AddProducts;
